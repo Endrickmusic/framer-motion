28768a57f38cdfb6c54d7785d4f9378ac56ad52c
Christian <mail@christianhohenbild.de>
2024-05-09 23:19:25 +0200

    add divs

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,41 @@ export default function App() {
 
  return (
 
+  <>
+    <div className="flex bg-gray-900 text-white h-screen">
+      <div className="flex bg-white text-black p-10 h-28 m-8">
+        DIV 1  
+      </div>
+      <div className="flex bg-yellow-500 text-black p-10 h-28 m-8">
+        DIV 2  
+      </div>
+      <div className="flex bg-green-500 text-black p-10 h-28 m-8">
+        DIV 3  
+      </div>
+      <div className="flex bg-orange-500 text-black p-10 h-28 m-8">
+        DIV 4  
+      </div>
+      <div className="flex bg-blue-500 text-black p-10 h-28 m-8">
+        DIV 5  
+      </div>
+      <div className="flex bg-purple-500 text-black p-10 h-28 m-8">
+        DIV 6  
+      </div>
+      <div className="flex bg-gray-500 text-black p-10 h-28 m-8">
+        DIV 7  
+      </div>
+      <div className="flex bg-lime-500 text-black p-10 h-28 m-8">
+        DIV 8  
+      </div>
+      <div className="flex bg-pink-500 text-black p-10 h-28 m-8">
+        DIV 9  
+      </div>
+      <div className="flex bg-teal-500 text-black p-10 h-28 m-8">
+        DIV 10  
+      </div>
+    </div>
+  </>
   
-    <Canvas shadows camera={{ position: [0, 0, 4], fov: 40 }}>
-      <Environment
-        files="./textures/envmap.hdr" />
-        <color 
-          attach="background" 
-          args={["#eeeeee"]} />
-      <Experience />
-    </Canvas>
-  
-  );
+  )
 }
 
