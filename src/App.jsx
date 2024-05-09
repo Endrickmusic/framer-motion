1dd91cd319480e74670de4da0e12a82e003747d7
Christian <mail@christianhohenbild.de>
2024-05-09 23:44:17 +0200

    add flex-grow layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,35 +11,36 @@ export default function App() {
  return (
 
   <>
-    <div className="flex bg-gray-900 text-white h-screen">
-      <div className="flex bg-white text-black p-10 h-28 m-8">
+    <div className="flex flex-wrap bg-gray-900 text-white h-screen">
+
+      <div className="flex flex-grow bg-white text-black items-center justify-center m-8 ">
         DIV 1  
       </div>
-      <div className="flex bg-yellow-500 text-black p-10 h-28 m-8">
+      <div className="flex bg-yellow-500 text-black items-center justify-center p-10 m-8 min-w-[200px]">
         DIV 2  
       </div>
-      <div className="flex bg-green-500 text-black p-10 h-28 m-8">
+      <div className="flex flex-grow bg-green-500 items-center justify-center text-black m-8">
         DIV 3  
       </div>
-      <div className="flex bg-orange-500 text-black p-10 h-28 m-8">
+      <div className="flex flex-shrink bg-orange-500 text-black items-center justify-center p-2 m-8">
         DIV 4  
       </div>
-      <div className="flex bg-blue-500 text-black p-10 h-28 m-8">
+      <div className="flex flex-grow bg-blue-500 text-black items-center justify-center p-2 m-8">
         DIV 5  
       </div>
-      <div className="flex bg-purple-500 text-black p-10 h-28 m-8">
+      <div className="flex bg-purple-500 text-black items-center justify-center p-2 m-8">
         DIV 6  
       </div>
-      <div className="flex bg-gray-500 text-black p-10 h-28 m-8">
+      <div className="flex flex-grow bg-gray-500 text-black items-center justify-center p-2 m-8">
         DIV 7  
       </div>
-      <div className="flex bg-lime-500 text-black p-10 h-28 m-8">
+      <div className="flex bg-lime-500 text-black items-center justify-center p-2 m-8">
         DIV 8  
       </div>
-      <div className="flex bg-pink-500 text-black p-10 h-28 m-8">
+      <div className="flex flex-grow bg-pink-500 text-black items-center justify-center p-2 m-8">
         DIV 9  
       </div>
-      <div className="flex bg-teal-500 text-black p-10 h-28 m-8">
+      <div className="flex flex-grow bg-teal-500 text-black items-center justify-center p-2 m-8">
         DIV 10  
       </div>
     </div>
