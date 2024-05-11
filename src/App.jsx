82c13226c3cedb2f209ce4b24811ff2abbe8bb3d
Christian <mail@christianhohenbild.de>
2024-05-11 15:51:55 +0200

    setup four divs

--- src/App.jsx
+++ src/App.jsx
@@ -31,17 +31,19 @@ export default function App() {
  return (
 
   <>
-    <div className="flex flex-wrap bg-gray-900 text-white h-full text-xl">
+    <div className="flex flex-wrap bg-gray-900 text-white h-full text-xl"
+    style={{ height: '200vh' }}
+    >
 
       <motion.div 
-        className="flex flex-grow flex-wrap bg-white text-black items-center m-8 p-8 z-10"
-        animate={{ backgroundColor: ["#FFFFFF", "#999", "#FFFFFF"] }}
+        className="flex flex-grow flex-wrap bg-white text-black items-center m-8 p-8 z-10 h-[300px]"
+        animate={{ }}
         transition={{
           type: "tween",
           ease: "easeInOut",
           repeat: Infinity, 
           duration: 10 }}
-        style={{ scale, y }}
+        style={{  }}
         >
         DIV 1
         <motion.div 
@@ -63,16 +65,9 @@ export default function App() {
         DIV 1.2 
         </motion.div>
 
-        <div className="flex flew-grow bg-blue-500 items-center justify-center m-4 p-4 w-full">
-        DIV 1.3 
-        </div>
-
-        <div className="flex bg-green-500 items-center justify-center m-4 p-4 w-full">
-        DIV 1.4 
-        </div>
       </motion.div>
 
-      <motion.div className="flex flex-wrap bg-yellow-500 text-black items-center p-10 m-8 min-w-[100px] z-20"
+      <motion.div className="flex flex-wrap bg-yellow-500 text-black items-center p-10 m-8 min-w-[100px] z-20 max-h-[300px]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ 
@@ -104,11 +99,8 @@ export default function App() {
         DIV 2.2 
         </motion.div>
 
-        <motion.div className="flex flew-grow bg-pink-500 items-center justify-center m-4 p-4 w-full min-h-[100px]">
-        DIV 2.3 
-        </motion.div>
       </motion.div>
-      <motion.div className="flex flex-grow bg-green-500 items-center justify-center text-black m-8"
+      <motion.div className="flex flex-grow bg-green-500 items-center justify-center text-black m-8 h-[300px]"
         initial={{ y: "-100vh" }}
         animate={{ y: 0 }}
         transition={{
@@ -120,79 +112,10 @@ export default function App() {
       >
         DIV 3  
       </motion.div>
-      <motion.div className="flex flex-shrink bg-orange-500 text-black items-center justify-center p-2 m-8">
+      <motion.div className="flex flex-shrink bg-orange-500 text-black items-center justify-center p-2 m-8 h-[300px]">
         DIV 4  
       </motion.div>
-      <motion.div className="flex flex-grow bg-blue-500 text-black items-center justify-center p-2 m-8"
-        initial={{ y: "-110vh" }}
-        animate={{ y: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 70,
-          duration: 5,
-          delay: 0,
-          }}
-      >
-        DIV 5  
-      </motion.div>
-      <motion.div className="flex bg-purple-500 text-black items-center justify-center p-2 m-8"
-        initial={{ y: "-90vh" }}
-        animate={{ y: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 70,
-          duration: 5,
-          delay: 0,
-          }}      
-      >
-        DIV 6  
-      </motion.div>
-      <motion.div className="flex flex-grow bg-gray-500 text-black items-center justify-center p-2 m-8"
-        initial={{ x: "-100vw"}}
-        animate={{ x: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 70,
-          duration: 15,
-          delay: 1
-          }}      
-      >
-        DIV 7  
-        <div className="flex flew-grow bg-yellow-500 items-center justify-center m-4 p-4 h-full">
-        DIV 7.1 
-        </div>
-
-        <div className="flex flew-grow bg-blue-500 items-center justify-center m-4 p-4 h-full">
-        DIV 7.2 
-        </div>
-
-        <div className="flex bg-green-500 items-center justify-center m-4 p-4 h-full">
-        DIV 7.3 
-        </div>
-      </motion.div>
-      <motion.div 
-      initial={{ rotateZ: 0 }}
-      animate={{ rotateZ: 360 }}
-      transition={{ 
-        type: "spring", 
-        ease: "easeInOut",
-        stiffness: 100, 
-        repeat: 2, 
-        duration: 15 }}
-      className="flex bg-lime-500 text-black items-center justify-center p-2 m-8">
-        DIV 8  
-      </motion.div>
-      <motion.div className="flex flex-grow bg-pink-500 text-black items-center justify-center p-6 m-8"
-        variants={container}
-        initial="hidden"
-        animate="show"
-      >
-        DIV 9 
-        <div className="flex bg-green-500 items-center justify-center h-full w-full">
-        DIV 9.1 
-        </div> 
-      </motion.div>
-
+    
     </div>
   </>
   
