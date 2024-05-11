92705affe2702d3a2fc029ca17a8371abec8f32f
Christian <mail@christianhohenbild.de>
2024-05-11 18:29:45 +0200

    add color change

--- src/App.jsx
+++ src/App.jsx
@@ -28,17 +28,42 @@ function Div({index}){
   const ref = useRef(null)
   const { scrollYProgress } = useScroll({ target: ref} )
   const y = useParallax(scrollYProgress, 300)
+  const ySlow = useParallax(scrollYProgress, 100)
+  const yFast = useParallax(scrollYProgress, 500)
+
+  // Interpolate scroll progress to a range of colors
+  const color = useTransform(scrollYProgress, [0.1, 1], ['#ff0000', '#0000ff'])
 
   return (
-    <section className="h-screen flex flex-col justify-center items-center relative scroll-snap-center perspective-500 bg-gray-900 text-white text-xl"
-    >
+    <section className="h-screen flex justify-center items-center relative perspective-500 bg-gray-900 text-white text-xl">
+      
+      <motion.div 
+        style={{ y: ySlow, backgroundColor: color }}
+        className="relative flex items-center justify-center w-[300px] h-[400px] bg-red-500"
+      >
+        Slow DIV {index}
+      </motion.div>
+      
       <div 
         ref={ref}
-        className="relative w-[300px] h-[400px] bg-white text-black m-20 z-10"
+        className="relative flex items-center justify-center w-[300px] h-[400px] bg-white text-black m-20 z-10"
         >
         DIV {index}
       </div>
-      <motion.h2 style={{ y }}>{`#00${index}`}</motion.h2>
+
+      <motion.div 
+        style={{ y: yFast, backgroundColor: color }}
+        className="relative flex items-center justify-center w-[300px] h-[400px] bg-blue-500 "
+      >
+        Fast DIV {index}
+      </motion.div>
+
+      <motion.h2 
+      style={{ y }}
+      className='m-8'
+      >
+        {`#00${index}`}
+      </motion.h2>
     
     </section>
   )
