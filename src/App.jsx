4d50cb4b963d3f7af392b1e376d2ec8a02641f75
Christian <mail@christianhohenbild.de>
2024-05-11 19:56:11 +0200

    add different count of slow, normal and fast divs

--- src/App.jsx
+++ src/App.jsx
@@ -6,65 +6,67 @@ import './index.css'
 
 export default function App() {
 
- return (
+  // Create arrays with 3, 7, and 5 elements
+  const slowDivs = new Array(3).fill(null);
+  const fastDivs = new Array(7).fill(null);
+  const normalDivs = new Array(5).fill(null);
 
-  <>
-    <div>
-    {[1, 2, 3, 4, 5].map((index) => (
-    <Div index={index} />
-))}
-    </div>
-  </>
-  
-  )
+  return (
+    <>
+      <div className="h-screen w-full overflow-auto scroll-snap-y-mandatory pb-20">
+        {/* Map over the slowDivs array to create the slow divs */}
+        {slowDivs.map((_, index) => (
+          <Div index={index} speed="slow" key={index} />
+        ))}
+        {/* Map over the normalDivs array to create the normal divs */}
+        {normalDivs.map((_, index) => (
+          <Div index={index} speed="normal" key={index} />
+        ))}
+        {/* Map over the fastDivs array to create the fast divs */}
+        {fastDivs.map((_, index) => (
+          <Div index={index} speed="fast" key={index} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 function useParallax(value, distance) {
   return useTransform(value, [0, 1], [-distance, distance]);
 }
 
-function Div({index}){
+function Div({index, speed}){
   
   const ref = useRef(null)
   const { scrollYProgress } = useScroll({ target: ref} )
-  const y = useParallax(scrollYProgress, 300)
+  const yNormal = useParallax(scrollYProgress, 300)
   const ySlow = useParallax(scrollYProgress, 100)
   const yFast = useParallax(scrollYProgress, 500)
 
   // Interpolate scroll progress to a range of colors
   const color = useTransform(scrollYProgress, [0.1, 1], ['#ff0000', '#0000ff'])
 
+  let y;
+  switch(speed) {
+    case "slow":
+      y = ySlow;
+      break;
+    case "fast":
+      y = yFast;
+      break;
+    default:
+      y = yNormal;
+  }
+
+
   return (
     <section className="h-screen flex justify-center items-center relative perspective-500 bg-gray-900 text-white text-xl">
-      
       <motion.div 
-        style={{ y: ySlow, backgroundColor: color }}
+        style={{ y, backgroundColor: color }}
         className="relative flex items-center justify-center w-[300px] h-[400px] bg-red-500"
       >
-        Slow DIV {index}
+        {speed.toUpperCase()} DIV {index}
       </motion.div>
-      
-      <div 
-        ref={ref}
-        className="relative flex items-center justify-center w-[300px] h-[400px] bg-white text-black m-20 z-10"
-        >
-        DIV {index}
-      </div>
-
-      <motion.div 
-        style={{ y: yFast, backgroundColor: color }}
-        className="relative flex items-center justify-center w-[300px] h-[400px] bg-blue-500 "
-      >
-        Fast DIV {index}
-      </motion.div>
-
-      <motion.h2 
-      style={{ y }}
-      className='m-8'
-      >
-        {`#00${index}`}
-      </motion.h2>
-    
     </section>
   )
 }
